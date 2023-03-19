98bf4ee91fb933936fc809b8da4ab115c89e0e84
Chiaosy Tan <ct00094@my.westga.edu>
2023-03-19 11:30:37 -0700

    LoadPatientListView method added

--- ClinicManagementApp/UserControls/SearchPatientUserControl.cs
+++ ClinicManagementApp/UserControls/SearchPatientUserControl.cs
@@ -40,7 +40,52 @@ namespace ClinicManagementApp.UserControls
 
         private void SetDefaultPageSetting()
         {
+            dobDateTimePicker.MaxDate = DateTime.Now.Date;
+            dobDateTimePicker.CustomFormat = "yyyy / MM / dd";
+            firstAndLastNameRadio.Checked = false;
+            lastDOBRadio.Checked = false;
+            dobRadio.Checked = false;
+            firstNameTextBox.Text = string.Empty;
+            firstNameLabel.Hide();
+            firstNameTextBox.Hide();
+            lastNameLabel.Hide();
+            lastNameTextBox.Text = string.Empty;
+            lastNameTextBox.Hide();
+            dobLabel.Hide();
+            dobDateTimePicker.Value = DateTime.Now.Date;
+            dobDateTimePicker.Hide();
+            this.messageBox.Text = "";
+            this.patientSearchButton.Visible = false;
+        }
 
+        /// <summary>
+        /// This method loads patient list in ListView
+        /// </summary>
+        /// <param name="firstNameIn">first name of patient</param>
+        /// <param name="lastNameIn">last name of patient</param>
+        /// <param name="dobIn">birthday of patient</param>
+        public void LoadPatientListView(string firstNameIn, string lastNameIn, DateTime dobIn)
+        {
+            List<Patient> patientFoundList = this._patientController.GetPatientByInputsCheck(firstNameIn, lastNameIn, dobIn);
+
+            if (patientFoundList.Count > 0)
+            {
+                this.messageBox.Text = "";
+                this.patientListView.Items.Clear();
+                Patient currentPatient;
+                for(int i = 0; i < patientFoundList.Count; i++)
+                {
+                    currentPatient = patientFoundList[i];
+                    patientListView.Items.Add(currentPatient.PatientID.ToString());
+                    patientListView.Items[i].SubItems.Add(currentPatient.LastName);
+                    patientListView.Items[i].SubItems.Add(currentPatient.FirstName);
+                    patientListView.Items[i].SubItems.Add(currentPatient.DateOfBirth.ToShortDateString());
+                }
+            }
+            else
+            {
+                this.messageBox.Text = "No patient found.";
+            }
         }
     }
 }
