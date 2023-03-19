e66f7dacf202d9fc8ffc0a3d8fd745914a2bcdae
Chiaosy Tan <ct00094@my.westga.edu>
2023-03-19 00:46:55 -0700

    added constructor in SearchPatientUserControl

--- ClinicManagementApp/UserControls/SearchPatientUserControl.cs
+++ ClinicManagementApp/UserControls/SearchPatientUserControl.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿
+using ClinicManagementApp.Controller;
+using ClinicManagementApp.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +13,34 @@ using System.Windows.Forms;
 
 namespace ClinicManagementApp.UserControls
 {
+    /// <summary>
+    /// this class manages usercontrol for viewing patient information
+    /// </summary>
     public partial class SearchPatientUserControl : UserControl
     {
+        private PatientController _patientController;
+
+        // 1. event - set variable to hold patient id
+        private Patient _controlNumber;
+
+        // 2. event - create customer event
+        public event EventHandler PatientNumberChanged;
+
+        /// <summary>
+        /// create constructors
+        /// </summary>
         public SearchPatientUserControl()
         {
             InitializeComponent();
+            this._patientController = new PatientController();
+            this.SetDefaultPageSetting();
+            this._controlNumber = new Patient();
+
+        }
+
+        private void SetDefaultPageSetting()
+        {
+
         }
     }
 }
