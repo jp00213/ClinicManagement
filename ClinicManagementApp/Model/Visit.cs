0b847313359cd55c8337f43a1128bb431ee39c34
Chiaosy Tan <ct00094@my.westga.edu>
2023-03-29 22:45:14 -0700

    created Visit Model

---
+++ ClinicManagementApp/Model/Visit.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicManagementApp.Model
+{
+    public class Visit
+    {
+        /// <summary>
+        /// The Visit model class.
+        /// </summary>
+        public int VisitID { get; set; }
+
+        public int AppointmentID { get; set; }
+
+        public int NurseID { get; set; }
+
+        public DateTime VisitDatetime { get; set; }
+
+        public decimal Height { get; set; }
+
+        public decimal Weight { get; set; }
+
+        public int BloodPressureDiastolic { get; set; }
+
+        public int BloodPressureSystolic { get; set; }
+
+        public decimal BodyTemperature { get; set; }
+
+        public int Pulse { get; set; }  
+
+        public string Symptoms { get; set; }
+
+        public string InitialDiagnoses { get; set; }
+
+        public string FinalDiagnoses { get; set; }
+
+        public string DoctorLastName { get; set; }
+
+        public string DoctorFirstName { get; set;}
+
+        public string NurseLastname { get; set; }
+
+        public string NurseFirstName { get; set;}
+
+        public string PatientLastName { get; set; }
+
+        public string PatientFirstName { get; set;}
+
+        public int PatientID { get; set; }
+
+        public string AppointmentReason { get; set; }   
+
+    }
+}
