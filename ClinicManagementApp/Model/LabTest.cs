ac7e041ac59d217a8a6273c0d8ee24b7c6ff9340
Chiaosy Tan <ct00094@my.westga.edu>
2023-04-03 20:05:18 -0700

    created LabTest Model

---
+++ ClinicManagementApp/Model/LabTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClinicManagementApp.Model
+{
+    /// <summary>
+    /// The LabTest model class.
+    /// </summary>
+    public class LabTest
+    {
+        public int VisitID { get; set; }
+
+        public int TestCode { get; set; }
+
+        public DateTime TestDate { get; set; }
+
+        public string Result { get; set; }
+
+        public string TestName { get; set; }
+
+        public int ResultIsNormal { get; set; }
+    }
+}
