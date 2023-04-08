bdb71b3da10864021abd728131c8c179540471a1
Amber Nicholas <anicho16@my.westga.edu>
2023-04-08 09:10:50 -0500

    Added nurse model. Added query to get nurse by id. Added nurse data object model.

---
+++ ClinicManagementApp/Model/Nurse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicManagementApp.Model
+{
+    /// <summary>
+    /// The Nurse model class.
+    /// </summary>
+    public class Nurse : Person
+    {
+        public int NurseID { get; set; }
+    }
+}
