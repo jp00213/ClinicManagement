8b76980617970b5b2e79811e1423eda1fc06768a
Amber Nicholas <anicho16@my.westga.edu>
2023-04-16 08:03:36 -0500

    Updated Admin Dashboard to reflect tab names in mock ups, docked tabs, increased dashboard size

--- ClinicManagementApp/View/AdministratorDashboard.Designer.cs
+++ ClinicManagementApp/View/AdministratorDashboard.Designer.cs
@@ -29,52 +29,59 @@
         private void InitializeComponent()
         {
             this.administratorTabControl = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.addNurseTabPage = new System.Windows.Forms.TabPage();
+            this.viewNurseTabPage = new System.Windows.Forms.TabPage();
             this.welcomeUserLabel = new System.Windows.Forms.Label();
             this.logoutLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.updateNurseTabPage = new System.Windows.Forms.TabPage();
             this.administratorTabControl.SuspendLayout();
             this.SuspendLayout();
             // 
             // administratorTabControl
             // 
-            this.administratorTabControl.Controls.Add(this.tabPage1);
-            this.administratorTabControl.Controls.Add(this.tabPage2);
+            this.administratorTabControl.Controls.Add(this.addNurseTabPage);
+            this.administratorTabControl.Controls.Add(this.viewNurseTabPage);
+            this.administratorTabControl.Controls.Add(this.updateNurseTabPage);
+            this.administratorTabControl.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.administratorTabControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.administratorTabControl.Location = new System.Drawing.Point(2, 73);
+            this.administratorTabControl.Location = new System.Drawing.Point(0, 58);
+            this.administratorTabControl.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.administratorTabControl.Name = "administratorTabControl";
             this.administratorTabControl.SelectedIndex = 0;
-            this.administratorTabControl.Size = new System.Drawing.Size(862, 630);
+            this.administratorTabControl.Size = new System.Drawing.Size(970, 512);
             this.administratorTabControl.TabIndex = 0;
             // 
-            // tabPage1
+            // addNurseTabPage
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 33);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(854, 593);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.addNurseTabPage.Location = new System.Drawing.Point(4, 27);
+            this.addNurseTabPage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.addNurseTabPage.Name = "addNurseTabPage";
+            this.addNurseTabPage.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.addNurseTabPage.Size = new System.Drawing.Size(962, 481);
+            this.addNurseTabPage.TabIndex = 0;
+            this.addNurseTabPage.Text = "Add Nurse";
+            this.addNurseTabPage.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // viewNurseTabPage
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(854, 601);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.viewNurseTabPage.Location = new System.Drawing.Point(4, 27);
+            this.viewNurseTabPage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.viewNurseTabPage.Name = "viewNurseTabPage";
+            this.viewNurseTabPage.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.viewNurseTabPage.Size = new System.Drawing.Size(962, 481);
+            this.viewNurseTabPage.TabIndex = 1;
+            this.viewNurseTabPage.Text = "View Nurse";
+            this.viewNurseTabPage.UseVisualStyleBackColor = true;
             // 
             // welcomeUserLabel
             // 
             this.welcomeUserLabel.AutoSize = true;
             this.welcomeUserLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
             this.welcomeUserLabel.ForeColor = System.Drawing.Color.DarkGreen;
-            this.welcomeUserLabel.Location = new System.Drawing.Point(34, 9);
+            this.welcomeUserLabel.Location = new System.Drawing.Point(26, 7);
+            this.welcomeUserLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.welcomeUserLabel.Name = "welcomeUserLabel";
-            this.welcomeUserLabel.Size = new System.Drawing.Size(115, 29);
+            this.welcomeUserLabel.Size = new System.Drawing.Size(91, 24);
             this.welcomeUserLabel.TabIndex = 3;
             this.welcomeUserLabel.Text = "Welcome";
             // 
@@ -82,22 +89,33 @@
             // 
             this.logoutLinkLabel.AutoSize = true;
             this.logoutLinkLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.logoutLinkLabel.Location = new System.Drawing.Point(744, 14);
+            this.logoutLinkLabel.Location = new System.Drawing.Point(886, 13);
+            this.logoutLinkLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.logoutLinkLabel.Name = "logoutLinkLabel";
-            this.logoutLinkLabel.Size = new System.Drawing.Size(68, 24);
+            this.logoutLinkLabel.Size = new System.Drawing.Size(54, 18);
             this.logoutLinkLabel.TabIndex = 4;
             this.logoutLinkLabel.TabStop = true;
             this.logoutLinkLabel.Text = "Logout";
             this.logoutLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.logoutLinkLabel_LinkClicked);
             // 
+            // updateNurseTabPage
+            // 
+            this.updateNurseTabPage.Location = new System.Drawing.Point(4, 27);
+            this.updateNurseTabPage.Name = "updateNurseTabPage";
+            this.updateNurseTabPage.Size = new System.Drawing.Size(962, 481);
+            this.updateNurseTabPage.TabIndex = 2;
+            this.updateNurseTabPage.Text = "Update Nurse";
+            this.updateNurseTabPage.UseVisualStyleBackColor = true;
+            // 
             // AdministratorDashboard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(862, 701);
+            this.ClientSize = new System.Drawing.Size(970, 570);
             this.Controls.Add(this.logoutLinkLabel);
             this.Controls.Add(this.welcomeUserLabel);
             this.Controls.Add(this.administratorTabControl);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AdministratorDashboard";
@@ -113,9 +131,10 @@
         #endregion
 
         private System.Windows.Forms.TabControl administratorTabControl;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage addNurseTabPage;
+        private System.Windows.Forms.TabPage viewNurseTabPage;
         private System.Windows.Forms.Label welcomeUserLabel;
         private System.Windows.Forms.LinkLabel logoutLinkLabel;
+        private System.Windows.Forms.TabPage updateNurseTabPage;
     }
 }
