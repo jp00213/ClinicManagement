852e6d9fc18da84213047835ed3dce1bd7e090e9
Jennifer Parks <jp00213@my.westga.edu>
2023-04-16 12:37:36 -0400

    Combined retrieving full name with method that retrieves Nurse object by username

--- ClinicManagementApp/DAL/NurseDAL.cs
+++ ClinicManagementApp/DAL/NurseDAL.cs
@@ -10,46 +10,6 @@ namespace ClinicManagementApp.DAL
     public class NurseDAL
     {
         /// <summary>
-        /// Returns full name of nurse by username
-        /// </summary>
-        /// <param name="user"></param>
-        /// <returns>Nurse full name</returns>
-        /// <exception cref="ArgumentOutOfRangeException"></exception>
-        public string GetNurseName(string user)
-        {
-            if (user == "" || user is null)
-            {
-                throw new ArgumentOutOfRangeException("user", "Username can't be blank or null");
-            }
-
-            string fullName = "";
-
-            string selectStatement =
-                "SELECT firstName, lastName " +
-                "FROM Person JOIN Nurse ON Person.recordID = Nurse.recordID " +
-                "WHERE Nurse.Username = @Username";
-
-            using (SqlConnection connection = ClinicManagementDBConnection.GetConnection())
-            {
-                connection.Open();
-                using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
-                {
-                    selectCommand.Parameters.Add("@Username", System.Data.SqlDbType.VarChar);
-                    selectCommand.Parameters["@Username"].Value = user;
-                    using (SqlDataReader reader = selectCommand.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            fullName = reader["firstName"].ToString() + " " + reader["lastName"].ToString();
-                        }
-                    }
-                }
-            }
-
-            return fullName;
-        }
-
-        /// <summary>
         /// Get nurse by id
         /// </summary>
         /// /// <param name="nurseID">id of nurse</param>
